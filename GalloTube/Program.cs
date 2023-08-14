b84e6beb2c3f8a53840355d007256981bf6bc5ab
José Francisco Dos Santos Neto <jf9718830@gmail.com>
2023-08-14 08:47:12 -0300

    Fiz até o program.cs

--- GalloTube/Program.cs
+++ GalloTube/Program.cs
@@ -6,8 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddTransient<IVideoTagRepository, VideoTagRepository>();
 builder.Services.AddTransient<ITagRepository, TagRepository>();
 builder.Services.AddTransient<IVideoRepository, VideoRepository>();
+
 
 var app = builder.Build();
 
